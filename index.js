b1fe38cc885056f0fc80adccbc2f6a7a84cb3414
kjgeer <75705035+kjgeer@users.noreply.github.com>
2021-01-20 12:49:57 -0600

    first problem

--- index.js
+++ index.js
@@ -45,8 +45,9 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
+function copy(){
+    
+  
 }    
 
 
@@ -160,8 +161,11 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(arr, string){
+    const newArray = [];
+    for(let i = 0; i<arr.length; i++){
+        if(arr)
+    }
 }
 
 
